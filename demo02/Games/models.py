78037b47a19255d4e3d9d90a5ea289cb7c444f29
dh <2964727542@qq.con>
2019-04-16 19:12:30 +0800

    demo02创建数据库

--- demo02/Games/models.py
+++ demo02/Games/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Games(models.Model):
+    gamename = models.CharField(max_length=20)
+    company = models.CharField(max_length=10)
+    issue_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.gamename
+
+
+class Protagonist(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.CharField(max_length=10)
+    undergo = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
